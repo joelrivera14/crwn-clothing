deedcf434ddbb62877fb5c81e4cb7b228e60535c
joel rivera <jorivera114@gmail.com>
2023-09-14 19:21:32 -0400

    sign up

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -2,15 +2,18 @@ import {
   signInWithGooglePopup,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
+import SignUpForm from "../../components/sign-up-from/sign-up-form.components";
 const Signin = () => {
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
   };
+
   return (
     <div>
       <h1>Sign in page</h1>
       <button onClick={logGoogleUser}>sign in with google</button>
+      <SignUpForm />
     </div>
   );
 };
